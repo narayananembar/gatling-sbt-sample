2c08758c2534816183af6d0d1aab0e9fd198a4cb
Andy Petrella <andy.petrella@gmail.com>
2012-07-20 17:37:55 +0200

    boh

--- project/Build.scala
+++ project/Build.scala
@@ -9,27 +9,11 @@ object MinimalBuild extends Build {
   val appName = "gatling-sbt-sample"
   val buildVersion =  "0.0.1-SNAPSHOT"
 
-  /* OFFICIAL GATLING REPO */
-  val gatlingReleasesRepo = "Gatling Releases Repo" at "http://repository.excilys.com/content/repositories/releases"
-  val gatling3PartyRepo = "Gatling Third-Party Repo" at "http://repository.excilys.com/content/repositories/thirdparty"
 
-  /* GATLING DEPS */
-  val gatlingVersionNumber = "1.2.5"
-  val gatlingApp = "com.excilys.ebi.gatling" % "gatling-app" % gatlingVersionNumber //withSources
-  val gatlingCore = "com.excilys.ebi.gatling" % "gatling-core" % gatlingVersionNumber //withSources
-  val gatlingHttp = "com.excilys.ebi.gatling" % "gatling-http" % gatlingVersionNumber //withSources
-  val gatlingRecorder = "com.excilys.ebi.gatling" % "gatling-recorder" % gatlingVersionNumber //withSources
-  val gatlingCharts = "com.excilys.ebi.gatling" % "gatling-charts" % gatlingVersionNumber //withSources
-  val gatlingHighcharts = "com.excilys.ebi.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersionNumber //withSources
-
+  /* LOCAL MAVEN REPO */
+  val localMavenRepo = "Local Maven Repository" at file(Path.userHome.absolutePath+"/.m2/repository").toURI.toURL.toString
 
   val libDependencies = Seq(
-     gatlingApp,
-     gatlingCore,
-     gatlingHttp,
-     gatlingRecorder,
-     gatlingCharts,
-     gatlingHighcharts,
 
     "be.nextlab" %% "gatling-sbt-test-framework" % "0.0.1-SNAPSHOT"
   )
@@ -42,7 +26,7 @@ object MinimalBuild extends Build {
       version := buildVersion,
       organization := "be.nextlab",
 
-      resolvers ++= Seq(gatlingReleasesRepo, gatling3PartyRepo),
+      // resolvers ++= Seq(gatlingReleasesRepo, gatling3PartyRepo),
       
       javacOptions += "-Xlint:unchecked",
       
@@ -52,5 +36,5 @@ object MinimalBuild extends Build {
     )
 
   lazy val root = Project(id = appName, base = file(".")).configs(GatlingTest).settings(allSettings: _*)
-
+  
 }
