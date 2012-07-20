c9ab54a0f5440f34f22a382562eb1f2992acfbf8
Andy Petrella <andy.petrella@gmail.com>
2012-07-20 23:31:51 +0200

    declare cloudbees as repo

--- project/Build.scala
+++ project/Build.scala
@@ -13,8 +13,16 @@ object MinimalBuild extends Build {
   /* LOCAL MAVEN REPO */
   val localMavenRepo = "Local Maven Repository" at file(Path.userHome.absolutePath+"/.m2/repository").toURI.toURL.toString
 
+  val gatSbtTestVersion = "0.0.1-SNAPSHOT"
+
+  val cloudbees = "https://repository-andy-petrella.forge.cloudbees.com/"
+  val cloudbeesRepo = gatSbtTestVersion match {
+      case x if x.endsWith(SNAPSHOT) => x.toLowerCase at cloudbees + "snapshot" + "/"
+      case x => x.toLowerCase at cloudbees + "release" + "/"
+  }
+
   val libDependencies = Seq(
-    "be.nextlab" %% "gatling-sbt-test-framework" % "0.0.1-SNAPSHOT" % "gatling-test"
+    "be.nextlab" %% "gatling-sbt-test-framework" % gatSbtTestVersion % "gatling-test"
   )
 
 
@@ -25,7 +33,7 @@ object MinimalBuild extends Build {
       version := buildVersion,
       organization := "be.nextlab",
 
-      // resolvers ++= Seq(gatlingReleasesRepo, gatling3PartyRepo),
+      resolvers ++= Seq(cloudbeesRepo),
       
       javacOptions += "-Xlint:unchecked",
       
